bc0a06e9297099b17a7de14eb5b29d55d2623d2e
David Tamaratare Oghenebrume <151965409+lil-brumski@users.noreply.github.com>
2024-05-19 18:38:00 +0100

    Update

---
+++ templates-to-use.cpp
@@ -0,0 +1,39 @@
+
+//A
+
+ template <class T>
+        T Input(const std::string& prompt){
+            T value;
+            while(true){
+                std::cout << prompt;
+                if(std::cin >> value){
+                    break;
+                }
+                else{
+                    std::cin.clear();
+                    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                    std::cerr << "Wrong data type, try again!\n";
+                }
+            }
+            return value;
+         }               
+
+
+
+//B
+     template <class T>
+       T Input(const std::string& prompt) {
+          T value;
+          std::string input;
+         while (true) {
+            std::cout << prompt;
+            std::getline(std::cin, input);
+            std::istringstream stream(input);
+            if (stream >> value && stream.eof()) {
+                 break;
+            } else {
+                 std::cerr << "Wrong data type, try again!\n";
+              }
+        }
+        return value;
+     }

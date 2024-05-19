a5a9035ac9a64b6b0025f59f03732a6323a074e6
David Tamaratare Oghenebrume <151965409+lil-brumski@users.noreply.github.com>
2024-05-19 18:49:28 +0100

    Update templates-to-use.cpp

--- templates-to-use.cpp
+++ templates-to-use.cpp
@@ -1,5 +1,5 @@
 
-//A
+//A- For int-bool.
 
  template <class T>
         T Input(const std::string& prompt){
@@ -20,7 +20,7 @@
 
 
 
-//B
+//B- For int, double and int-bool(can use it for string-boolean when std::boolalpha is used.
      template <class T>
        T Input(const std::string& prompt) {
           T value;
@@ -37,3 +37,22 @@
         }
         return value;
      }
+
+
+//C- For char type.
+        template <class T>
+        T Input(const std::string& prompt){
+            T value;
+            while(true){
+                std::cout << prompt;
+                if(std::cin >> value && std::cin.get() == '\n'){
+                    break;
+                }
+                else{
+                    std::cin.clear();
+                    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                    std::cerr << "Wrong data type, try again!\n";
+                }
+            }
+            return value;
+         }               
